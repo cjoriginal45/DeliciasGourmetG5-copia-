a0723552967b006b8a5f3f1c89b35f473220b748
cjoriginal45 <joaquinribarola45@gmail.com>
2024-08-22 13:30:24 -0300

    modelado de capas

---
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/persistencia/ControlPersistencia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo5.taller.restaurantdeliciasgourmet.persistencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ControlPersistencia {
+    
+}
