95f3e749bf1a60eb15199f41dfe91201df6d3f5d
cjoriginal45 <joaquinribarola45@gmail.com>
2024-08-22 13:39:37 -0300

    enums

---
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Permiso.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo5.taller.restaurantdeliciasgourmet.logica;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum Permiso {
+    PERMISO_COMPLETO,
+    PERMISO_LIMITADO;
+}
