e6cd43c8130dbb55509f01d059ee57c909b4aec6
cjoriginal45 <joaquinribarola45@gmail.com>
2024-08-22 14:14:31 -0300

    Empleado

--- src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Rol.java
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Rol.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package grupo5.taller.restaurantdeliciasgourmet.logica;
 
-/**
- *
- * @author Usuario
- */
+
 public enum Rol {
     ADMIN,RECEPCIONISTA,MOZO,MAITRE;
 }
