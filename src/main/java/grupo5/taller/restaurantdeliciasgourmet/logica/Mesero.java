063c2452a5ca340d58c5f6dcc8b28afda57efdfa
cjoriginal45 <joaquinribarola45@gmail.com>
2024-08-22 11:55:42 -0300

    clases

--- src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Mesero.java
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Mesero.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package grupo5.taller.restaurantdeliciasgourmet.logica;
 
 /**
  *
  * @author Usuario
  */
-public class Empleado {
-    
+public class Mesero {
+    private String ala;
 }
