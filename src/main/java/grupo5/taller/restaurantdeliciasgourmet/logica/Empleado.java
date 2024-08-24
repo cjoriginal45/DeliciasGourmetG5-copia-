7dc16f0ce4040280c495f56df7a335fa383f1ce0
cjoriginal45 <joaquinribarola45@gmail.com>
2024-08-24 12:19:24 -0300

    subir

--- src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Empleado.java
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Empleado.java
@@ -9,29 +9,7 @@ package grupo5.taller.restaurantdeliciasgourmet.logica;
  * @author Usuario
  */
 public class Empleado {
-    private int kl;
-    private int juan [] = new int[10];
-
-    public Empleado(int kl) {
-        this.kl = kl;
-    }
-
-    public int getKl() {
-        return kl;
-    }
-
-    public void setKl(int kl) {
-        this.kl = kl;
-    }
-
-    public int[] getJuan() {
-        return juan;
-    }
-
-    public void setJuan(int[] juan) {
-        this.juan = juan;
-    }
-    
+  
     
     
 }
