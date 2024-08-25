18b6040505337c55f2a03b8a68211de46752da7c
Equipo <Equipo@DESKTOP-PPCHE8B>
2024-08-25 13:35:19 -0300

    clase maitre

---
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/Maitre.java
@@ -0,0 +1,17 @@
+
+package grupo5.taller.restaurantdeliciasgourmet.logica;
+
+
+public class Maitre extends Empleado {
+    
+    public void coordinarConCocina(){
+    
+    }
+    public void supervisarPersonal(){
+    
+    }
+    public void gestionarQuejas(){
+    
+    }
+    
+}
