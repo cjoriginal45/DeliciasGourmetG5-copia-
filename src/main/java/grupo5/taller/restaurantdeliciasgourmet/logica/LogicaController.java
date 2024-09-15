acfe8a2f143dce50c36712312fc29d556d23435e
cjoriginal45 <joaquinribarola45@gmail.com>
2024-09-15 18:10:00 -0300

    Creacion de la controladoras logica y de persistencia y metodos CRUD

--- src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/LogicaController.java
+++ src/main/java/grupo5/taller/restaurantdeliciasgourmet/logica/LogicaController.java
@@ -2,126 +2,103 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package grupo5.taller.restaurantdeliciasgourmet.persistencia;
+package grupo5.taller.restaurantdeliciasgourmet.logica;
 
-import grupo5.taller.restaurantdeliciasgourmet.logica.Cliente;
-import grupo5.taller.restaurantdeliciasgourmet.logica.Empleado;
-import grupo5.taller.restaurantdeliciasgourmet.logica.Mesa;
-import grupo5.taller.restaurantdeliciasgourmet.logica.Reserva;
+import grupo5.taller.restaurantdeliciasgourmet.persistencia.PersistenceController;
 import java.util.ArrayList;
 
 /**
  *
- * @author sebas
+ * @author Usuario
  */
-public class PersistenceController {
-    EmpleadoJpaController empleadoJpaController = new EmpleadoJpaController();
+public class LogicaController {
 
-    
+    PersistenceController control = new PersistenceController();
+
     /*----------------Metodos CRUD--------------------------------*/
-    
-    
-    /*----------Empleado-----------------*/
-    
+ /*----------Empleado-----------------*/
     public void crearEmpleado(Empleado emp) {
-        
+        control.crearEmpleado(emp);
     }
 
     public void eliminarEmpleado(int id) {
-
+        control.eliminarEmpleado(id);
     }
 
     public void editarEmpleado(Empleado emp) {
-
+        control.editarEmpleado(emp);
     }
 
     public Empleado traerEmpleado(int id) {
-
-        return null;
-
+        return control.traerEmpleado(id);
     }
 
     public ArrayList<Empleado> ListaEmpleado() {
-
-        return null;
-
+        return control.ListaEmpleado();
     }
 
     /*--------------Reserva----------------*/
-    
     public void crearReserva(Reserva res) {
-
+        control.crearReserva(res);
     }
 
     public void eliminarReserva(int id) {
-
+        control.eliminarReserva(id);
     }
 
     public void editarReserva(Reserva res) {
-
+        control.editarReserva(res);
     }
 
     public Reserva traerReserva(int id) {
-
-        return null;
-
+        return control.traerReserva(id);
     }
 
     public ArrayList<Reserva> ListaReserva() {
-
-        return null;
-
+        return control.ListaReserva();
     }
-    
-    /*--------------Cliente----------------*/
-    
-     public void crearCliente(Cliente cli) {
 
-     }
+    /*--------------Cliente----------------*/
+    public void crearCliente(Cliente cli) {
+        control.crearCliente(cli);
+    }
 
     public void eliminarCliente(int id) {
-
+        control.eliminarCliente(id);
     }
 
     public void editarCliente(Cliente cli) {
-
+        control.editarCliente(cli);
     }
 
     public Cliente traerCliente(int id) {
-
-        return null;
-
+        return control.traerCliente(id);
     }
 
     public ArrayList<Cliente> ListaCliente() {
-
-        return null;
-
+        return control.ListaCliente();
     }
-
-//  
-
-    public void crearMesa(Mesa mesa) {
-
+    
+    /*--------------Mesa----------------*/
+        public void crearMesa(Mesa mesa) {
+        control.crearMesa(mesa);
     }
 
     public void eliminarMesa(int id) {
-
+        control.eliminarMesa(id);
     }
 
     public void editarMesa(Mesa mesa) {
-
+        control.editarMesa(mesa);
     }
 
     public Cliente traerMesa(int id) {
-       return null;
+        return control.traerMesa(id);
     }
 
     public ArrayList<Mesa> ListaMesa() {
-        return null;
+        return control.ListaMesa();
     }
-
-   
-
+    
     
 }
